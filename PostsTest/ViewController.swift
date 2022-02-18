bca410df033401d591b7e9902cff835f99c3e095
Gulzat <gulzat97@mail.ru>
2022-02-18 09:26:27 +0600

    Initial Commit

---
+++ PostsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsTest
+//
+//  Created by Guzlat on 18/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
